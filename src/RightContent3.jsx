7ca3518f92f8124d19f805932431a73ce70b94ed
Can <Mehmet-CanZ@web.de>
2024-04-06 19:16:01 +0200

    Added Redbull

--- src/RightContent3.jsx
+++ src/RightContent3.jsx
@@ -12,9 +12,10 @@ const RightContent3 = ({ content }) => {
       <p>Ayran türk. Joghurtgetränk<sup>g</sup><span className='spanStyle'>1,00</span><span className='spanStyle'>0,33l&nbsp; </span></p>
       <p className='pExtraStyle2'>Mineralwasser<sup></sup><span className='spanStyle'>1,50</span><span className='spanStyle'>0,33l&nbsp; </span></p>
       <p>Tetra Pak verschiedene Sorten<sup></sup><span className='spanStyle'>1,50</span><span className='spanStyle'>0,5l&nbsp; </span></p>
-      <p className='pExtraStyle2'>Tasse Kaffee<sup>8</sup><span className='spanStyle'>2,00</span><span className='spanStyle'>&nbsp; </span></p>
-      <p>Bier alle Sorten<sup>g</sup><span className='spanStyle'>3,00</span><span className='spanStyle'>0,5l&nbsp; </span></p>
-      <p className='pExtraStyle2'>Bier alle Sorten<sup>g</sup><span className='spanStyle'>2,70</span><span className='spanStyle'>0,33l&nbsp; </span></p>
+      <p className='pExtraStyle2'>Red Bull verschiedene Sorten<sup></sup><span className='spanStyle'>2,50</span><span className='spanStyle'>0,25l&nbsp; </span></p>
+      <p>Tasse Kaffee<sup>8</sup><span className='spanStyle'>2,00</span><span className='spanStyle'>&nbsp; </span></p>
+      <p className='pExtraStyle2'>Bier alle Sorten<sup>g</sup><span className='spanStyle'>3,00</span><span className='spanStyle'>0,5l&nbsp; </span></p>
+      <p>Bier alle Sorten<sup>g</sup><span className='spanStyle'>2,70</span><span className='spanStyle'>0,33l&nbsp; </span></p>
    
     </div>
   );
